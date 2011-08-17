ccf9ade29dac9afff810e6785e01e167e047437a
Toby Clemson <tobyclemson@gmail.com>
2011-08-17 23:59:29 +0100

    Adding empty leiningen project.

---
+++ src/lifestyle_recordings/core.clj
@@ -0,0 +1,1 @@
+(ns lifestyle-recordings.core)
---
+++ test/lifestyle_recordings/test/core.clj
@@ -0,0 +1,6 @@
+(ns lifestyle-recordings.test.core
+  (:use [lifestyle-recordings.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
